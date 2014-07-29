18c566801fbe49a8380d11c01af9d19b2f61e4da
Sharnie Ivery <sharnieivery@gmail.com>
2014-07-29 14:27:56 -0400

    Created message model, views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
+  get 'messages/show'
+
+  get 'messages/new'
+
+  get 'messages/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
