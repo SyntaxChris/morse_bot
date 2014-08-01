21f529bb74c4ce92e0c141e1f7485d3c3d1dde77
Chris Lehneis <cdlehneis@gmail.com>
2014-08-01 15:49:44 -0400

    implemented unique user seach on new message view

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -20,9 +20,21 @@ $(document).on('page:change', function(){
         // console.log(response);
 
         $.each(response, function(index, user){
-          $('ul#user-results').html('<li>'+user.email+'</li>');
+          // $('ul#user-results').html('<li>'+user.email+'</li>');
+
+          $('ul#user-results').html('<li>'+user.email+'</li>').prototype.getUnique = function(){
+            var u = {}, a= [];
+            for(var i = 0, l = this.length; i < l; ++i){
+              if(u.hasOwnProperty(this[i])){
+                continue;
+              }
+              a.push(this[i]);
+              u[this[i]] = 1;
+            }
+            return a;
+          }
         });
-        
+
       });
 
     }
