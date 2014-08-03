bfc67508a175387e99d97db35aa25cc57d29eb30
Chris Lehneis <cdlehneis@gmail.com>
2014-08-03 15:15:07 -0400

    added username sign in to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,20 @@ class User < ActiveRecord::Base
   has_many :messages
   has_many :messages, through: :message_recipients
 
+
+  attr_accessor :login
+
   def self.find_user query
     self.where("email LIKE ?", "%#{query}%")
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
   end
 
 end
